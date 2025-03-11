b7693ab7c6ea0a940c0e91bdde9881510081870b
Hossein Amer <hossein.esmat@gmail.com>
2025-03-11 13:00:33 +0100

    Removed react-slidedown from FaqItem.jsx and package.json

--- OneDrive/Desktop/itadproject/xora/src/components/FaqItem.jsx
+++ OneDrive/Desktop/itadproject/xora/src/components/FaqItem.jsx
@@ -1,6 +1,5 @@
 import clsx from "clsx";
 import { useState } from "react";
-import { SlideDown } from "react-slidedown";
 import "react-slidedown/lib/slidedown.css";
 
 const FaqItem = ({ item, index }) => {
